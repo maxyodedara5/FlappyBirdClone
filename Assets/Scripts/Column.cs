2343ca9ffa777d4ff0b316a3bb849c10e733689b
maxyodedara5 <maxyodedara5@gmail.com>
2017-08-21 12:39:17 +0530

    Added the whole game
    Added all the files in the desktop of Flappy Bird game with the Unity scene and assets folder

---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour {
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird>() != null)
+        {
+            GameControl.instance.BirdScored();
+        }
+    }
+
+}
---
+++ README.txt.txt
@@ -0,0 +1,10 @@
+Controls for game 
+Left Click for flying
+
+For playing the game 
+Make sure you have downloaded the FlappyBird_data folder
+and FlappyBird.exe file (only downloading this 2 items is enough for playing the game)
+Simply run the exe file and game will start 
+
+To close the game 
+"Alt + F4"
